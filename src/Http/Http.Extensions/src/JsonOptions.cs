43c2a2b890377884942108b527c077a092ae50c7
Bruno Oliveira <brolivei@microsoft.com>
2022-12-14 09:50:01 -0800

    Fixing AOT warnings

--- src/Http/Http.Extensions/src/JsonOptions.cs
+++ src/Http/Http.Extensions/src/JsonOptions.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics.CodeAnalysis;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
@@ -16,8 +15,7 @@ namespace Microsoft.AspNetCore.Http.Json;
 /// </summary>
 public class JsonOptions
 {
-    //TEMP: Public for troubleshooting
-    public static readonly JsonSerializerOptions DefaultSerializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web)
+    internal static readonly JsonSerializerOptions DefaultSerializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web)
     {
         // Web defaults don't use the relex JSON escaping encoder.
         //
@@ -28,7 +26,9 @@ public class JsonOptions
         // The JsonSerializerOptions.GetTypeInfo method is called directly and needs a defined resolver
         // setting the default resolver (reflection-based) but the user can overwrite it directly or calling
         // .AddContext<TContext>()
-        TypeInfoResolver = CreateDefaultTypeResolver()
+#pragma warning disable IL2026 // Disabling IL2026 since JsonTrimmability.IsTrimmable is a feature switch and adding RequiresUnreferencedCode or UnconditionalSuppressMessage is not right since we would like to have it reported to users is the default value is changed 
+        TypeInfoResolver = JsonTrimmability.IsTrimmable ? null : new DefaultJsonTypeInfoResolver()
+#pragma warning restore IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
     };
 
     // Use a copy so the defaults are not modified.
@@ -36,10 +36,4 @@ public class JsonOptions
     /// Gets the <see cref="JsonSerializerOptions"/>.
     /// </summary>
     public JsonSerializerOptions SerializerOptions { get; } = new JsonSerializerOptions(DefaultSerializerOptions);
-
-    //[UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026",
-    //    Justification = "The default type resolver will be set as DefaultJsonTypeInfoResolver (reflection-based) that has the same runtime behavior when TypeResolver is null.")]
-    [RequiresUnreferencedCode("Calls System.Text.Json.Serialization.Metadata.DefaultJsonTypeInfoResolver.DefaultJsonTypeInfoResolver()")]
-    private static IJsonTypeInfoResolver? CreateDefaultTypeResolver()
-        => JsonTrimmability.IsTrimmable ? null : new DefaultJsonTypeInfoResolver();
 }
