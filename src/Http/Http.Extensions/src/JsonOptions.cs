a795023270223792e945cb9ba10c4d44688d3424
Bruno Oliveira <brolivei@microsoft.com>
2022-12-15 10:37:17 -0800

    Adding RUC

--- src/Http/Http.Extensions/src/JsonOptions.cs
+++ src/Http/Http.Extensions/src/JsonOptions.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
@@ -15,7 +16,7 @@ namespace Microsoft.AspNetCore.Http.Json;
 /// </summary>
 public class JsonOptions
 {
-    internal static readonly JsonSerializerOptions DefaultSerializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web)
+    public static readonly JsonSerializerOptions DefaultSerializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web)
     {
         // Web defaults don't use the relex JSON escaping encoder.
         //
@@ -26,8 +27,8 @@ public class JsonOptions
         // The JsonSerializerOptions.GetTypeInfo method is called directly and needs a defined resolver
         // setting the default resolver (reflection-based) but the user can overwrite it directly or calling
         // .AddContext<TContext>()
-#pragma warning disable IL2026 // Disabling IL2026 since JsonTrimmability.IsTrimmable is a feature switch and adding RequiresUnreferencedCode or UnconditionalSuppressMessage is not right since we would like to have it reported to users is the default value is changed 
-        TypeInfoResolver = JsonTrimmability.IsTrimmable ? null : new DefaultJsonTypeInfoResolver()
+#pragma warning disable IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
+        TypeInfoResolver = JsonTrimmability.IsTrimmable ? null : CreateDefaultResolver()
 #pragma warning restore IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
     };
 
@@ -36,4 +37,7 @@ public class JsonOptions
     /// Gets the <see cref="JsonSerializerOptions"/>.
     /// </summary>
     public JsonSerializerOptions SerializerOptions { get; } = new JsonSerializerOptions(DefaultSerializerOptions);
+
+    [RequiresUnreferencedCode("Calls System.Text.Json.Serialization.Metadata.DefaultJsonTypeInfoResolver.DefaultJsonTypeInfoResolver()")]
+    private static IJsonTypeInfoResolver CreateDefaultResolver() => new DefaultJsonTypeInfoResolver();
 }
